059f9747fadefe37abba149ef0de62f09fef93dd
Mohammed <arrahmaan9901@gmail.com>
2022-07-05 06:40:46 +0530

    created data file

---
+++ src/store/pdf.js
@@ -0,0 +1,8 @@
+export const projects = {
+    "Projects": {
+        "Kamrani%20Kaizen": [
+            require("../img/projects/kamraniKaizenTirunelveli/kamrani kaizen Brochure.pdf"),
+
+        ],
+    }
+}
