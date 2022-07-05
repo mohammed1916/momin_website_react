67062ab67623a82469574cd4a104aa47cfabc7e4
Mohammed <arrahmaan9901@gmail.com>
2022-07-05 17:47:29 +0530

    created data

---
+++ src/store/address.js
@@ -0,0 +1,8 @@
+export const address = {
+    "Momin%20Elan": {
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d638.3166393323631!2d77.60446183549567!3d12.987215362984873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb01b02e967f3817f!2zMTLCsDU5JzE0LjIiTiA3N8KwMzYnMTcuMSJF!5e0!3m2!1sen!2sin!4v1646714920974!5m2!1sen!2sin",
+        street: " No.56/1, Old Bamboo Bazaar Road, Shivajinagar,",
+        city: "Bangalore - 560051.",
+    },
+
+}
