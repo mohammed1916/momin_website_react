c1626851aca77b6496363f675e5bce90824a3d40
Mohammed <arrahmaan9901@gmail.com>
2022-07-05 17:04:42 +0530

    accomplished specification list

--- src/Components/Specifications/Specifications.js
+++ src/Components/Specifications/Specifications.js
@@ -32,6 +32,10 @@ import { Specifications } from '../../store/specifications.js'
 
 const cards = [kamraniKaizenTirunelveli, basavanagudi, greenEnclave, bangloreTelephoneLayout, chennaiRoopasResidency, gangaNagarAslamsResidence, hbr, hbr1, lingarajpuram, loydsroadbanglore, pearlNestvadapalinichennai, kamanahali, yeshwanthpur, mosqueroadbengalore, interior];
 
+const bg = [
+    'radial-gradient(#CEE26B, #5CB270)',
+]
+
 
 function getURLUsername(siteName) {
     const n = siteName.lastIndexOf('/');
@@ -52,7 +56,7 @@ export default function Specification() {
 
     return (
         <>
-            <div className="bg-gray-100">
+            <div className="bg-gray-100 pb-7">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <section id="section1" class="topOff">
                         <div class="container">
@@ -61,28 +65,26 @@ export default function Specification() {
                                     <h1 class="text-center text-cyan-800 text-4xl pt-12">Project Specifications</h1>
                                     <p class="text-center text-cyan-500 text-2xl pt-1 font-extralight">Bringing it all together!</p>
                                 </div>
-                                <div class="flex flex-wrap justify-items-stretch justify-center pt-8">
-                                    {(typeof (Specifications[siteURLName]) !== 'undefined')
-                                        ?
-                                        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 
-                                            {Specifications[siteURLName].map((object, index) => (
-                                                <div class="card card-1">
-                                                    <div class="card__icon">
-                                                    </div>
-                                                    <h2 class="card__description">{object["description"]} </h2>
-                                                    <p class="card__title">
-                                                        <a class="card__link" href="#">{object["title"]}</a>
-                                                    </p>
-                                                </div>
-                                            ))}
+                                {(typeof (Specifications[siteURLName]) !== 'undefined')
+                                    ?
+                                    <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-10 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
 
-                                        </div>
-                                        :
-                                        <div>
-                                        </div>
-                                    }
-                                </div>
+                                        {Specifications[siteURLName].map((object, index) => (
+
+                                            <div class={`card-${index % 5} rounded-lg p-5`}>
+                                                <p class="card__title text-cyan-900 text-xl pb-3">
+                                                    <a class="card__link" href="#">{object["title"]}</a>
+                                                </p>
+                                                <h2 class="card__description text-white">{object["description"]} </h2>
+                                            </div>
+                                        ))}
+
+                                    </div>
+                                    :
+                                    <div>
+                                    </div>
+                                }
                             </div>
                         </div>
                     </section>
