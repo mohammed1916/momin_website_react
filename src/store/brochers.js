4c88aca5aea7f593067609f585e9179c5dee68b5
Mohammed <arrahmaan9901@gmail.com>
2022-07-05 10:55:41 +0530

    added data

---
+++ src/store/brochers.js
@@ -0,0 +1,17 @@
+export const brocher = {
+    "brocher": {
+        "Kamrani%20Kaizen": [
+            require("../img/projects/kamraniKaizenTirunelveli/G.E. - Broucher PG1.jpg"),
+            require("../img/projects/kamraniKaizenTirunelveli/G.E. - Broucher PG2.jpg"),
+        ],
+        "Green%20Enclave": [
+            require("../img/projects/greenEnclave/brochers/G.E. - Broucher PG1.jpg"),
+            require("../img/projects/greenEnclave/brochers/G.E. - Broucher PG1.jpg"),
+        ],
+        "Pearl%20Nest": [
+            require("../img/projects/pearlNestvadapalinichennai/G1.jpeg"),
+            require("../img/projects/pearlNestvadapalinichennai/G2.jpeg"),
+            require("../img/projects/pearlNestvadapalinichennai/pearlNest.jpeg"),
+        ],
+    }
+}
