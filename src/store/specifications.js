23e2d6dd7b36cccd280b25eec99b5b6f5e59e42c
Mohammed <arrahmaan9901@gmail.com>
2022-07-05 14:48:15 +0530

    data for specifications

---
+++ src/store/specifications.js
@@ -0,0 +1,50 @@
+export const Specification = {
+    "Specification": {
+        "Momin%20Elan": [
+            {
+                title: "Structure",
+                description: "RCC framed structure with columns and beams. Walls of concrete solid blocks plastered on both sides."
+            },
+            {
+                title: "Flooring",
+                description: "Lobby, stair case and lift area will be provided with granite. Living area and bedrooms will be privided with vetrified flooring. Antiskid ceramic tiles in toilets."
+            },
+            {
+                title: "Doors",
+                description: "All doors will be flush doors. Main door will be solid panelled teak wood or equivalent."
+            },
+            {
+                title: "Windows",
+                description: "High quality powder coated aluminium windows with MS grills."
+            },
+            {
+                title: "Water Supply",
+                description: "Borewell alongwith Corporatoin water."
+            },
+            {
+                title: "Power Backup",
+                description: "Diesel Generator will be provided for lift and common areas only."
+            },
+            {
+                title: "Electrical",
+                description: "Modular Swithches, Anchor make or equivalent will be provided."
+            },
+            {
+                title: "Wall and External Finishes",
+                description: "All internal walls will be painted with emulsion. All external walls will be painted with ACE."
+            },
+            {
+                title: "Kitchen",
+                description: "2 feet dadooing above 20mm thick granite counter with edge polish. Provision for water purifier."
+            },
+            {
+                title: "Bathroom",
+                description: "Ceramic tiles upto 7 feet height, Cp fittings, sanityware of Parryware/ equivalent."
+            },
+            {
+                title: "Lift",
+                description: "One fully automatic modern lift of six passengers capacity."
+            },
+        ],
+    }
+}
