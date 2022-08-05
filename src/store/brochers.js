93e1f968ed4683963c0de32bb85bbf9975493450
Mohammed <arrahmaan9901@gmail.com>
2022-08-05 22:01:27 +0530

    Update brochers.js

--- src/store/brochers.js
+++ src/store/brochers.js
@@ -1,17 +1,17 @@
 export const brocher = {
     "brocher": {
         "Kamrani%20Kaizen": [
-            require("../img/projects/kamraniKaizenTirunelveli/G.E. - Broucher PG1.jpg"),
-            require("../img/projects/kamraniKaizenTirunelveli/G.E. - Broucher PG2.jpg"),
+            require("../img/projects/kamraniKaizenTirunelveli/G.E. - Broucher PG1 Large.jpeg"),
+            require("../img/projects/kamraniKaizenTirunelveli/G.E. - Broucher PG2 Large.jpeg"),
         ],
         "Green%20Enclave": [
-            require("../img/projects/greenEnclave/broucher/G.E. - Broucher PG1.jpg"),
-            require("../img/projects/greenEnclave/broucher/G.E. - Broucher PG1.jpg"),
+            require("../img/projects/greenEnclave/broucher/G.E. - Broucher PG1 Large.jpeg"),
+            require("../img/projects/greenEnclave/broucher/G.E. - Broucher PG1 Large.jpeg"),
         ],
         "Pearl%20Nest": [
-            require("../img/projects/pearlNestvadapalinichennai/G1.jpeg"),
-            require("../img/projects/pearlNestvadapalinichennai/G2.jpeg"),
-            require("../img/projects/pearlNestvadapalinichennai/pearlNest.jpeg"),
+            require("../img/projects/pearlNestvadapalinichennai/G1Large.jpeg"),
+            require("../img/projects/pearlNestvadapalinichennai/G2Large.jpeg"),
+            require("../img/projects/pearlNestvadapalinichennai/pearlNestLarge.jpeg"),
         ],
     }
 }
