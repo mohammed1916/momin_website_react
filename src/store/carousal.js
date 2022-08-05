0491cbd0ec1214c48f289f7cdb7583161f112736
Mohammed <arrahmaan9901@gmail.com>
2022-08-05 15:06:19 +0530

    removed decoration image form carousal data

--- src/store/carousal.js
+++ src/store/carousal.js
@@ -1,13 +1,8 @@
 export const carousal = {
     "Momin%20Elan": [
         {
-            label: 'Momin Developers',
-            imgPath: require("../img/decoration/decoration1.png")
-        },
-        {
             label: 'Momin Elan',
             imgPath: require("../img/decoration/decoration2.png"),
-
         },
     ],
 
