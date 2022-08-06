637d57e029c3601ac2aeefa8b444e75ade122c84
Mohammed <arrahmaan9901@gmail.com>
2022-08-06 18:26:41 +0530

    Updated Data

--- src/store/carousal.js
+++ src/store/carousal.js
@@ -2,6 +2,36 @@ export const carousal = {
     "Momin%20Elan": [
         {
             label: 'Momin Elan',
+            imgPath: require("../img/elevation/elevation.jpeg"),
+        },
+        {
+            label: 'Momin Elan',
+            imgPath: require("../img/elevation/tiltBlack.jpeg"),
+        },
+        {
+            label: 'Momin Elan',
+            imgPath: require("../img/elevation/tiltWhite.jpeg"),
+        },
+    ],
+
+}
+export const elevationPlan = {
+    "Momin%20Elan": [
+        {
+            label: 'Momin Elan',
+            imgPath: require("../img/elevation/1BAMLarge.jpeg"),
+        },
+        {
+            label: 'Momin Elan',
+            imgPath: require("../img/elevation/2BAMLarge.jpeg"),
+        },
+    ],
+
+}
+export const img = {
+    "Momin%20Elan": [
+        {
+            label: 'Momin Elan',
             imgPath: require("../img/decoration/decoration2.png"),
         },
     ],
