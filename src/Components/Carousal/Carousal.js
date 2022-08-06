664f8b317be1ddd513a98100dbcfc1698c0c527e
Mohammed <arrahmaan9901@gmail.com>
2022-08-06 19:24:00 +0530

    Made Carausal work with component

--- src/Components/Carousal/Carousal.js
+++ src/Components/Carousal/Carousal.js
@@ -9,7 +9,8 @@ import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
-import { carousal } from '../../store/carousal.js'
+import { carousal, elevationPlan, elevationImg } from '../../store/carousal.js'
+import CarousalComponent from './CarousalComponent.js';
 
 
 function getURLUsername(siteName) {
@@ -19,86 +20,13 @@ function getURLUsername(siteName) {
 
 export default function Carousal() {
     var siteURLName = getURLUsername(window.location.pathname);
-    // console.log("carousal[siteURLName]", carousal[siteURLName]);
-
-    const theme = useTheme();
-    const [activeStep, setActiveStep] = React.useState(0);
-
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleStepChange = (step) => {
-        setActiveStep(step);
-    };
+    var data = carousal[siteURLName];
+    var data1 = elevationPlan[siteURLName];
+    var data2 = elevationImg[siteURLName];
     return (
         <>
-            {(typeof (carousal[siteURLName]) !== 'undefined')
-                ?
-                <section className="flex justify-center p-6">
-                    <Box sx={{ maxWidth: 1080 }}>
-                        <MobileStepper
-                            steps={carousal[siteURLName].length}
-                            position="static"
-                            activeStep={activeStep}
-                            nextButton={
-                                <Button
-                                    size="small"
-                                    onClick={handleNext}
-                                    disabled={activeStep === carousal[siteURLName].length - 1}
-                                >
-                                    Next
-                                    {theme.direction === 'rtl' ? (
-                                        <KeyboardArrowLeft />
-                                    ) : (
-                                        <KeyboardArrowRight />
-                                    )}
-                                </Button>
-                            }
-                            backButton={
-                                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                                    {theme.direction === 'rtl' ? (
-                                        <KeyboardArrowRight />
-                                    ) : (
-                                        <KeyboardArrowLeft />
-                                    )}
-                                    Back
-                                </Button>
-                            }
-                        />
-                        <SwipeableViews
-                            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                            index={activeStep}
-                            onChangeIndex={handleStepChange}
-                            enableMouseEvents
-                        >
-                            {carousal[siteURLName].map((step, index) => (
-                                <div key={step.label}>
-                                    {Math.abs(activeStep - index) <= 2 ? (
-                                        <Box
-                                            component="img"
-                                            sx={{
-                                                display: 'block',
-                                                overflow: 'hidden',
-                                            }}
-                                            src={step.imgPath}
-                                            alt={step.label}
-                                        />
-                                    ) : null}
-                                </div>
-                            ))}
-                        </SwipeableViews>
-
-                    </Box>
-                </section>
-                :
-                <div>
-                </div>
-            }
+            <CarousalComponent data={data} maxWidth={1080} />
+            <CarousalComponent data={data1} maxWidth={591} />
         </>
     );
 }
