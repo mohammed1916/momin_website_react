2018c488b187ec6498258d4e19f497685c78394c
Mohammed <arrahmaan9901@gmail.com>
2022-08-06 18:27:16 +0530

    Removed Auto swipe, Made swipable

--- src/Components/Carousal/Carousal.js
+++ src/Components/Carousal/Carousal.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box'
 import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import { carousal } from '../../store/carousal.js'
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 function getURLUsername(siteName) {
     const n = siteName.lastIndexOf('/');
@@ -14,8 +19,18 @@ function getURLUsername(siteName) {
 
 export default function Carousal() {
     var siteURLName = getURLUsername(window.location.pathname);
+    // console.log("carousal[siteURLName]", carousal[siteURLName]);
 
+    const theme = useTheme();
     const [activeStep, setActiveStep] = React.useState(0);
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
 
     const handleStepChange = (step) => {
         setActiveStep(step);
@@ -25,21 +40,60 @@ export default function Carousal() {
             {(typeof (carousal[siteURLName]) !== 'undefined')
                 ?
                 <section className="flex justify-center p-6">
-                    {carousal[siteURLName].map((step, index) => (
-                        <div key={step.label}>
-                            {Math.abs(activeStep - index) <= 2 ? (
-                                <Box
-                                    component="img"
-                                    sx={{
-                                        display: 'block',
-                                        overflow: 'hidden',
-                                    }}
-                                    src={step.imgPath}
-                                    alt={step.label}
-                                />
-                            ) : null}
-                        </div>
-                    ))}
+                    <Box sx={{ maxWidth: 1080 }}>
+                        <MobileStepper
+                            steps={carousal[siteURLName].length}
+                            position="static"
+                            activeStep={activeStep}
+                            nextButton={
+                                <Button
+                                    size="small"
+                                    onClick={handleNext}
+                                    disabled={activeStep === carousal[siteURLName].length - 1}
+                                >
+                                    Next
+                                    {theme.direction === 'rtl' ? (
+                                        <KeyboardArrowLeft />
+                                    ) : (
+                                        <KeyboardArrowRight />
+                                    )}
+                                </Button>
+                            }
+                            backButton={
+                                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                                    {theme.direction === 'rtl' ? (
+                                        <KeyboardArrowRight />
+                                    ) : (
+                                        <KeyboardArrowLeft />
+                                    )}
+                                    Back
+                                </Button>
+                            }
+                        />
+                        <SwipeableViews
+                            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                            index={activeStep}
+                            onChangeIndex={handleStepChange}
+                            enableMouseEvents
+                        >
+                            {carousal[siteURLName].map((step, index) => (
+                                <div key={step.label}>
+                                    {Math.abs(activeStep - index) <= 2 ? (
+                                        <Box
+                                            component="img"
+                                            sx={{
+                                                display: 'block',
+                                                overflow: 'hidden',
+                                            }}
+                                            src={step.imgPath}
+                                            alt={step.label}
+                                        />
+                                    ) : null}
+                                </div>
+                            ))}
+                        </SwipeableViews>
+
+                    </Box>
                 </section>
                 :
                 <div>
